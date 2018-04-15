bf27a67e60f0284dfb1c175075fe4b3527ecd96d
Kamil Kapalka <k.kapalka@hotmail.com>
2018-04-15 14:08:38 +0200

    all scenes except inventory

---
+++ Assets/Scripts/LocationSelectionScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocationSelectionScript : MonoBehaviour {
+	
+	GameObject[] FightSelection;
+	// Use this for initialization
+	void Start () {
+		FightSelection = GameObject.FindGameObjectsWithTag ("FightSelection");
+		foreach (GameObject go in FightSelection) {
+			go.SetActive (false);
+		}
+		GameObject.Find ("Next Mission").SetActive (false);
+	}
+	
+	public void ZoneChosen(GameObject toActivate){
+		foreach (GameObject go in FightSelection) {
+			go.SetActive (false);
+		}
+		toActivate.SetActive (true);
+	}
+	public void Embark(){
+		SceneManager.LoadScene ("Fight");
+	}
+	public void ReturnToMainMenu(){
+		SceneManager.LoadScene ("Main Menu");
+	}
+}
--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour {
 
 	public void QuitGame(){
 		Debug.Log ("Game has just quitted.");
 		Application.Quit ();
+	}
+	public void LoadCredits(){
+		SceneManager.LoadScene ("Credits");
+	}
+	public void LoadGame(){
+		SceneManager.LoadScene ("Location Selection");
+	}
+	public void StartNewGame(){
+		SceneManager.LoadScene ("Story");
 	}
 }
