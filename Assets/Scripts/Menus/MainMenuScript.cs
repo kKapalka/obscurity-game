6f2a62a989f34f9462b94ed67efe7a0c62470740
Kamil Kapalka <k.kapalka@hotmail.com>
2018-05-24 05:04:34 +0200

    main menu cleanup

--- Assets/Scripts/Menus/MainMenuScript.cs
+++ Assets/Scripts/Menus/MainMenuScript.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+using System.IO;
+
 public class MainMenuScript : MonoBehaviour {
 
-
+	public GameObject loadGame;
+	void Start(){
+		if (!File.Exists (Application.persistentDataPath + "/Inventory.dat") || !File.Exists (Application.persistentDataPath + "/Equipment.dat"))
+			loadGame.SetActive(false);
+	}
+	public void ClearAllSavedData(){
+		if (File.Exists (Application.persistentDataPath + "/Inventory.dat"))
+			File.Delete (Application.persistentDataPath + "/Inventory.dat");
+		if (File.Exists (Application.persistentDataPath + "/Equipment.dat"))
+			File.Delete (Application.persistentDataPath + "/Equipment.dat");
+	}
 	public void QuitGame(){
 		Debug.Log ("Game has just quitted.");
 		Application.Quit ();
